fe0cf3833fb4436af3e05ac54d787c523d861919
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-26 10:13:57 -0600

    stub out resque call in spec

--- spec/jobs/poll_twilio_job_spec.rb
+++ spec/jobs/poll_twilio_job_spec.rb
@@ -3,13 +3,16 @@ require 'spec_helper'
 describe PollTwilioJob do
   describe '.perform' do
     it 'retrieves all phone numbers' do
+      Resque.stub(:enqueue)
       Elefeely.should_receive(:retrieve_phone_numbers).and_return(['1'])
+
       PollTwilioJob.perform
     end
 
     it 'enqueues job to send text' do
       Elefeely.stub(:retrieve_phone_numbers).and_return(['1234567890'])
       Resque.should_receive(:enqueue).with(SendSmsJob, '1234567890')
+
       PollTwilioJob.perform
     end
   end
