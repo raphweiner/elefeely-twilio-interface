c6eaa7af01a883bfde71a3a38c0d5da4c719b55d
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-26 10:10:21 -0600

    add dotenv, twilio to send text job

--- app/jobs/send_text_job.rb
+++ app/jobs/send_text_job.rb
@@ -2,6 +2,19 @@ class SendTextJob
   @queue = :send_text_queue
 
   def self.perform(phone_number)
-    Elefeely.send_text(phone_number)
+    client = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'],
+                                      ENV['TWILIO_AUTH_TOKEN'])
+
+    send_sms(client, phone_number)
+  end
+
+private
+
+  def self.send_sms(client, phone_number)
+    client.account.sms.messages.create(
+      :from => ENV['TWILIO_PHONE_NUMBER'],
+      :to => phone_number,
+      :body => 'How are you feeling? 1-Sad, 2-Tired, 3-Okay, 4-Good, 5-Awesome'
+    )
   end
 end
