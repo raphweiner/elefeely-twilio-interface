40a0743c5fc91dd245979d61d1f7aa00009a4d79
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-26 10:10:21 -0600

    add dotenv, twilio to send text job

--- spec/jobs/send_text_job_spec.rb
+++ spec/jobs/send_text_job_spec.rb
@@ -2,8 +2,18 @@ require 'spec_helper'
 
 describe SendTextJob do
   describe '.perform' do
-    it 'sends one text' do
-      Elefeely.should_receive(:send_text).with('1234567890')
+    it 'instantiates a new twilio client' do
+      SendTextJob.stub(:send_sms)
+      Twilio::REST::Client.should_receive(:new).with(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+
+      SendTextJob.perform('1234567890')
+    end
+
+    it 'sends an sms' do
+      mock_client = mock('twilio client')
+      Twilio::REST::Client.stub(:new).and_return(mock_client)
+      SendTextJob.should_receive(:send_sms).with(mock_client, '1234567890')
+
       SendTextJob.perform('1234567890')
     end
   end
