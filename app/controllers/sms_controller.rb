cc952d414620a3e1489b195b0213307237a44b0a
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-26 19:18:30 -0600

    big refactor. functionality the same

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -1,6 +1,14 @@
 class SmsController < ApplicationController
   def create
-    feely = Feely.new_and_send(params)
-    render :xml => {:Sms => feely.message}.to_xml(:root => 'Response')
+    sms_message = TwilioFeelingSMS.new(params)
+
+    if sms_message.valid?
+      Elefeely.create(source: 'twilio',
+                      event_id: sms_message.sms_sid,
+                      score: sms_message.body,
+                      uid: sms_message.phone_number)
+    end
+
+    render xml: sms_message.response_xml
   end
 end
--- spec/controllers/sms_controller_spec.rb
+++ spec/controllers/sms_controller_spec.rb
@@ -1,24 +1,36 @@
 require 'spec_helper'
 
 describe SmsController do
-  describe 'POST #create' do
-    before(:each) do
-      @params ||= YAML.load_file(Rails.root.join('spec/support', 'twilio_callback.yml'))
-    end
+  before(:all) do
+    @params = YAML.load_file(Rails.root.join('spec/support', 'twilio_callback.yml'))
+  end
 
+  describe 'POST #create' do
     context 'happy path' do
       it 'creates request to Elefeely API' do
-        feely_mock = mock('feely')
-        feely_mock.stub(:message)
-        Feely.should_receive(:new_and_send).and_return(feely_mock)
+        Elefeely.should_receive(:create)
 
         post :create, @params
       end
 
-      it 'returns with appropriate response' do
+      it 'returns an appropriate response' do
         post :create, @params
 
         expect(response.body).to eq "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n  <Sms>Great to hear!</Sms>\n</Response>\n"
+      end
+    end
+
+    context 'sad path' do
+      it 'creates request to Elefeely API' do
+        Elefeely.should_not_receive(:create)
+
+        post :create, @params.merge("Body" => "I'm invalid")
+      end
+
+      it 'returns an appropriate response' do
+        post :create, @params.merge("Body" => "I'm invalid")
+
+        expect(response.body).to eq "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<Response>\n  <Sms>Please enter a number 1-5</Sms>\n</Response>\n"
       end
     end
   end
