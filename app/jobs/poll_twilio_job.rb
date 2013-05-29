ec744f9b756a252f817ccd87aef1e0b354d5a0c4
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-29 13:35:16 -0600

    remove elefeely api request class and move to gem

--- app/jobs/poll_twilio_job.rb
+++ app/jobs/poll_twilio_job.rb
@@ -2,9 +2,7 @@ class PollTwilioJob
   @queue = :twilio_queue
 
   def self.perform
-    phone_numbers = ElefeelyAPIRequest.phone_numbers
-
-    enqueue_smss(phone_numbers)
+    enqueue_smss(Elefeely.phone_numbers)
   end
 
 private
