d49c694d3e3a57262451094846007a19af14fc83
Raphael Weiner <raphael.weiner@gmail.com>
2013-05-30 14:45:00 -0600

    fix twilio spec

--- spec/jobs/poll_twilio_job_spec.rb
+++ spec/jobs/poll_twilio_job_spec.rb
@@ -4,13 +4,13 @@ describe PollTwilioJob do
   describe '.perform' do
     it 'retrieves all phone numbers from the ElefeelyAPIRequest' do
       PollTwilioJob.stub(:enqueue_smss)
-      Elefeely.should_receive(:phone_numbers)
+      Elefeely.should_receive(:phone_numbers).and_return('abc')
 
       PollTwilioJob.perform
     end
 
     it 'enqueues job to send text' do
-      Elefeely.stub(:phone_numbers).and_return(['1234567890'])
+      Elefeely.stub(:phone_numbers).and_return('phone_numbers' => ['1234567890'])
       Resque.should_receive(:enqueue).with(SendSmsJob, :feeler, '1234567890')
 
       PollTwilioJob.perform
