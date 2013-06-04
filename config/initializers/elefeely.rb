8e9eeaec57c4f5025eaeaf4aee406a43eac14b0c
Raphael Weiner <raphael.weiner@gmail.com>
2013-06-04 10:14:42 -0600

    update configuration to match new gem config

--- config/initializers/elefeely.rb
+++ config/initializers/elefeely.rb
@@ -1,4 +1,1 @@
-Elefeely.configure do |config|
-  config.source_key = ENV['ELEFEELY_KEY']
-  config.source_secret = ENV['ELEFEELY_SECRET']
-end
+Elefeely.configure(source_key: ENV['ELEFEELY_KEY'], source_secret: ENV['ELEFEELY_SECRET'])
