6dc43a060faf7f43840f1fff14425a4b880f9495
Raphael Weiner <raphael.weiner@gmail.com>
2013-06-04 21:16:49 -0600

    pull out send sms feeler

--- app/controllers/sms_controller.rb
+++ app/controllers/sms_controller.rb
@@ -2,15 +2,22 @@ class SmsController < ApplicationController
   before_filter :validate_request, only: :verify
 
   def create
-    sms_response = ResponseViaTwilioSMS.new(params)
-    sms_response.forward
+    @sms_response = ResponseViaTwilioSMS.new(params)
+    @sms_response.forward
+    send_sms_feeler_if_verifying
 
-    Resque.enqueue(SendSmsJob, :feeler, phone_number) if sms_response.verifying?
-
-    render xml: sms_response.reply_xml
+    render xml: @sms_response.reply_xml
   end
 
   def verify
     Resque.enqueue(SendSmsJob, :verification, params[:number])
+  end
+
+private
+
+  def send_sms_feeler_if_verifying
+    if @sms_response.verifying?
+      Resque.enqueue(SendSmsJob, :feeler, @sms_response.phone_number)
+    end
   end
 end
