acc36f173049f424d58e75ff84186d605d46ded4
Raphael Weiner <raphael.weiner@gmail.com>
2013-06-06 01:14:54 -0600

    update elefeely configure

--- config/initializers/elefeely.rb
+++ config/initializers/elefeely.rb
@@ -1,1 +1,1 @@
-Elefeely.configure(source_key: ENV['ELEFEELY_KEY'], source_secret: ENV['ELEFEELY_SECRET'])
+Elefeely.configure(source_key: ENV['ELEFEELY_KEY'], source_secret: ENV['ELEFEELY_SECRET'], api_host: 'http://elefeely-api.herokuapp.com')
